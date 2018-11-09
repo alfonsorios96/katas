207d80d078dc6895e770c7c96c6ef2caa6b7448e
Alfonso Rios <malforime@gmail.com>
2018-11-09 15:18:58 -0600

    Add solutions, it is not optim

--- src/sudokuSolver/index.js
+++ src/sudokuSolver/index.js
@@ -1,19 +1,164 @@
 'use strict';
 
+const validator = require('../sudokuSolutionValidator');
+
 const solverSudoku = matrix => {
-    for (const row of matrix) {
-        const missingNumbers = getNumbersMissing(row);
-        const options = getOptions(row, missingNumbers);
+    const options = getAllOptions(matrix);
+    const matrices = combineOptions(options);
+    console.log(matrices);
+};
+
+const combineOptions = options => {
+    const matrices = [];
+
+    const paths = generatePaths(options);
+    let counter = 0;
+    do {
+        const matrix = [];
+        for (const option of options) {
+            const position = paths[counter];
+            while (option[position] === undefined) {
+                counter++;
+            }
+            matrix.push(option[position]);
+            counter++;
+        }
+        if (validator(matrix)) {
+            matrices.push(matrix);
+        }
+    } while (counter < paths.length);
+
+    return matrices;
+};
+
+const generatePaths = options => {
+    let paths = [];
+    let major = options.reduce((accumulator, iterator) => {
+        if (Array.isArray(accumulator)) {
+            return accumulator.length < iterator.length ? iterator.length : accumulator.length;
+        } else {
+            return accumulator < iterator.length ? iterator.length : accumulator;
+        }
+    });
+    const numbers = [];
+    major--;
+    while (major >= 0) {
+        numbers.push(major);
+        major--;
+    }
+    for (const permutation of getPermutationsWithRepeats(numbers, options.length)) {
+        paths.push(permutation);
+    }
+    return paths;
+};
+
+// PERMUTATION GENERATOR ------------------------------
+
+// getPermutationsWithRepeats :: [a] -> Int -> Generator [a]
+function* getPermutationsWithRepeats(xs, intGroup) {
+    const
+        vs = Array.from(xs),
+        intBase = vs.length,
+        intSet = Math.pow(intBase, intGroup);
+    if (0 < intBase) {
+        let index = 0;
+        while (index < intSet) {
+            const
+                ds = unfoldr(
+                    v => 0 < v ? (() => {
+                        const rd = quotRem(v, intBase);
+                        return Just(Tuple(vs[rd[1]], rd[0]))
+                    })() : Nothing(),
+                    index++
+                );
+            yield replicate(
+                intGroup - ds.length,
+                vs[0]
+            ).concat(ds);
+        }
+
+    }
+}
+
+// GENERIC FUNCTIONS ----------------------------------
+
+// Just :: a -> Maybe a
+const Just = x => ({
+    type: 'Maybe',
+    Nothing: false,
+    Just: x
+});
+
+// Nothing :: Maybe a
+const Nothing = () => ({
+    type: 'Maybe',
+    Nothing: true,
+});
+
+// Tuple (,) :: a -> b -> (a, b)
+const Tuple = (a, b) => ({
+    type: 'Tuple',
+    '0': a,
+    '1': b,
+    length: 2
+});
+
+// concat :: [[a]] -> [a]
+// concat :: [String] -> String
+const concat = xs =>
+    0 < xs.length ? (() => {
+        const unit = 'string' !== typeof xs[0] ? (
+            []
+        ) : '';
+        return unit.concat.apply(unit, xs);
+    })() : [];
+
+// index (!!) :: [a] -> Int -> a
+// index (!!) :: String -> Int -> Char
+const index = (xs, i) => xs[i];
+
+// quotRem :: Int -> Int -> (Int, Int)
+const quotRem = (m, n) =>
+    Tuple(Math.floor(m / n), m % n);
+
+// replicate :: Int -> a -> [a]
+const replicate = (n, x) =>
+    Array.from({
+        length: n
+    }, () => x);
+
+// show :: a -> String
+const show = x => JSON.stringify(x);
+
+// toLower :: String -> String
+const toLower = s => s.toLocaleLowerCase();
+
+// unfoldr(x => 0 !== x ? Just([x, x - 1]) : Nothing(), 10);
+// --> [10,9,8,7,6,5,4,3,2,1]
+
+// unfoldr :: (b -> Maybe (a, b)) -> b -> [a]
+const unfoldr = (f, v) => {
+    let
+        xr = [v, v],
+        xs = [];
+    while (true) {
+        const mb = f(xr[1]);
+        if (mb.Nothing) {
+            return xs
+        } else {
+            xr = mb.Just;
+            xs.push(xr[0])
+        }
     }
 };
 
-const getPermutations = array => {
+const getPermutationsNoRepeats = array => {
     if (array.length === 1) {
         return [array];
     }
     let permutationsResult = [];
 
-    const smallerPermutations = getPermutations(array.slice(1));
+    const smallerPermutations = getPermutationsNoRepeats(array.slice(1));
 
     const firstOption = array[0];
 
@@ -31,15 +176,28 @@ const getPermutations = array => {
 
 };
 
-const getOptions = (row, missingNumbers) => {
-    const permutations = getPermutations(missingNumbers);
-    for (const permutation of permutations) {
-        /*
-        * for (const number of missingNumbers) {
-            permutation.splice(row.indexOf(0), 1, number);
-        }*/
-        console.log(permutation);
+const getAllOptions = matrix => {
+    const options = [];
+    for (const row of matrix) {
+        const alternatives = getOptionsByRow(row);
+        options.push(alternatives);
     }
+    return options;
+};
+
+const getOptionsByRow = row => {
+    const result = [];
+    const missingNumbers = getNumbersMissing(row);
+    const permutations = getPermutationsNoRepeats(missingNumbers);
+    const originalRow = row.slice();
+    for (const permutation of permutations) {
+        for (const number of permutation) {
+            row.splice(row.indexOf(0), 1, number);
+        }
+        result.push(row);
+        row = originalRow.slice();
+    }
+    return result;
 };
 
 const getNumbersMissing = row => {
